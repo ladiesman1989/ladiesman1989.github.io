e5bc249b244522af6b83390f4c26a4e90fca7992
ladiesman1989 <72322945+ladiesman1989@users.noreply.github.com>
2021-04-29 14:46:12 +0100

    added head.php file

---
+++ head.php
@@ -0,0 +1,14 @@
+<?php echo'
+   <div class="home container-float">
+   <h1>Tourist attractions in China</h1>
+   <p class="ul1">
+       <a href="index.php">Home</a>&nbsp;&nbsp;
+       <a href="about.php"target="blank">About</a>&nbsp;&nbsp;
+       <a href="beijing.php" target="blank">Beijing</a>&nbsp;&nbsp;
+       <a href="shanghai.php" target="blank">Shanghai</a>&nbsp;&nbsp;
+       <a href="contact.php" target="blank">Contact</a>&nbsp;&nbsp;
+       <a href="https://www.w3schools.com/html/" target="blank">Help</a>&nbsp;&nbsp;
+   </p>  
+</div>';
+?>
+
